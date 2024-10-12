490dfa417fb51bd5096f40edad0258b2a41b6ef6
sam-part <124842992+sam-part@users.noreply.github.com>
2024-10-12 17:04:14 -0400

    Create timer struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod timer;
+
+use crate::timer::Timer;
+
+/*
+
+ui/
+layout/
+
+data/
+ mod.rs
+ serialization.rs
+ session.rs
+ statistics.rs
+ timer.rs
+
+app.rs
+config.rs
+errors.rs
+events.rs
+input.rs
+tests/
+
+*/
+
 fn main() {
-    println!("Hello, world!");
+    let mut timer: Timer = Timer::new();
+
+    timer.start();
+    timer.stop();
+
+    println!("Elapsed: {}", timer.elapsed().as_secs_f64());
 }
