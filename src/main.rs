61ff35f89c551b24fd8ad1753ff4c760ff1c1119
sam-part <124842992+sam-part@users.noreply.github.com>
2024-10-14 20:58:23 -0400

    Create tui instance

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,59 @@
-mod timer;
+use std::io::Stdout;
 
-use crate::timer::Timer;
+use color_eyre::eyre::Result;
+use ratatui::{crossterm, prelude::CrosstermBackend, widgets::Paragraph, Terminal};
 
-/*
+pub mod event;
+use event::{EventHandler, Message};
 
-ui/
-layout/
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
 
-data/
- mod.rs
- serialization.rs
- session.rs
- statistics.rs
- timer.rs
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
 
-app.rs
-config.rs
-errors.rs
-events.rs
-input.rs
-tests/
+    let event_handler = EventHandler::new(5.0);
+    let app_result = run(terminal, event_handler).await;
 
-*/
+    ratatui::restore();
 
-fn main() {
-    let mut timer: Timer = Timer::new();
+    app_result
+}
 
-    timer.start();
-    timer.stop();
+pub type Tui = Terminal<CrosstermBackend<Stdout>>;
 
-    println!("Elapsed: {}", timer.elapsed().as_secs_f64());
+async fn run(mut terminal: Tui, mut event_handler: event::EventHandler) -> Result<()> {
+    let mut counter: u64 = 0;
+
+    loop {
+        let hello_message = String::from("Hello CubeGen! ");
+
+        terminal.draw(|frame| {
+            let message = hello_message + &counter.to_string();
+            frame.render_widget(Paragraph::new(message), frame.area());
+        })?;
+
+        let message = event_handler.next().await?;
+
+        match message {
+            Message::Key(key) => {
+                if key.kind == crossterm::event::KeyEventKind::Press {
+                    if key.code == crossterm::event::KeyCode::Char('q') {
+                        break;
+                    }
+
+                    if key.code == crossterm::event::KeyCode::Char('x') {
+                        counter = 0;
+                    }
+                }
+            }
+
+            Message::Tick => counter += 1,
+
+            _ => {}
+        }
+    }
+
+    Ok(())
 }
